28d685048479dcd5d3bd35b4bf17b0cfb9201251
dmiv <dmitriy40647274@gmail.com>
2025-06-25 20:15:04 +0300

    errs

--- backend/app/api/dependencies.py
+++ backend/app/api/dependencies.py
@@ -3,6 +3,6 @@ from typing import Annotated
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from db.dependencies import get_async_db
+from app.db.dependencies import get_async_db
 
 async_db_dep = Annotated[AsyncSession, Depends(get_async_db)]

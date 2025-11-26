d19fcd70954676b2d862d3338b11a018ae4f4f82
Sergey Sapach <vip.sapach16@gmail.com>
2025-11-26 17:40:45 +0300

    A4-page wrapper

--- frontend/components/templates/Order.vue
+++ frontend/components/templates/Order.vue
@@ -4,6 +4,7 @@ import { usePurchasesStore } from '~/stores/purchases';
 import { useSalesStore } from '~/stores/sales';
 import type { Product, Person, GoodsDeal, ServicesDeal } from '~/types/dealState';
 import { injectionKeys, RequestedType } from '~/constants/keys';
+import A4Page from '../ui/A4-page.vue';
 
 const purchasesStore = usePurchasesStore()
 const salesStore = useSalesStore()
@@ -66,9 +67,9 @@ watch(() => insertState.value,
 			insertState.value.salesStateService = false
 		}
 
-		if ((requestedData === RequestedType.PURCHASES_GOOD || requestedData === RequestedType.SALES_GOOD) 
-		&& lastGoodsDeal) {
-			
+		if ((requestedData === RequestedType.PURCHASES_GOOD || requestedData === RequestedType.SALES_GOOD)
+			&& lastGoodsDeal) {
+
 			products = lastGoodsDeal.goods.goodsList?.map((product: Product) => ({
 				name: product.name,
 				article: product.article,
@@ -94,9 +95,9 @@ watch(() => insertState.value,
 			}
 		}
 
-		if ((requestedData === RequestedType.PURCHASES_SERVICE || requestedData === RequestedType.SALES_SERVICE) 
-		&& lastServicesDeal) {
-			
+		if ((requestedData === RequestedType.PURCHASES_SERVICE || requestedData === RequestedType.SALES_SERVICE)
+			&& lastServicesDeal) {
+
 			products = lastServicesDeal.services.servicesList?.map((product: Product) => ({
 				name: product.name,
 				article: product.article,
@@ -130,7 +131,7 @@ const changeState = inject(injectionKeys.changeStateOrderKey, ref(false))
 watch(() => changeState.value,
 	async () => {
 		if (requestedData === RequestedType.PURCHASES_GOOD) {
-			 await purchasesStore.fullUpdateGoodsDeal(
+			await purchasesStore.fullUpdateGoodsDeal(
 				orderData.value.orderNumber,
 				saller,
 				buyer,
@@ -139,7 +140,7 @@ watch(() => changeState.value,
 
 			orderData.value.amount = await purchasesStore?.lastGoodsDeal?.goods.amountPrice
 			orderData.value.amountWord = await purchasesStore?.lastGoodsDeal?.goods.amountWord
-			
+
 		} else if (requestedData === RequestedType.PURCHASES_SERVICE) {
 			await purchasesStore.fullUpdateServicesDeal(
 				orderData.value.orderNumber,
@@ -177,13 +178,12 @@ watch(() => changeState.value,
 	{ deep: true }
 )
 
-const element: Ref<HTMLElement | null> = useState('htmlOrder', () => ref(null))
 
 const addProduct = () => {
-	const productType: string = 
-	(requestedData === RequestedType.PURCHASES_GOOD || requestedData === RequestedType.SALES_GOOD) 
-	? 'товар' 
-	: 'услуга'
+	const productType: string =
+		(requestedData === RequestedType.PURCHASES_GOOD || requestedData === RequestedType.SALES_GOOD)
+			? 'товар'
+			: 'услуга'
 
 	const product: Product = {
 		name: '',
@@ -198,7 +198,7 @@ const addProduct = () => {
 	orderData.value.products.push(product)
 }
 
-const isDisabled = inject(injectionKeys.isDisabledKey, ref(true)) 
+const isDisabled = inject(injectionKeys.isDisabledKey, ref(true))
 
 
 //clear form button
@@ -263,118 +263,126 @@ const removeProduct = (product: any): void => {
 	const index = orderData.value.products.indexOf(product)
 	orderData.value.products.splice(index, 1)
 }
+
+const element: Ref<HTMLElement | null> = useState('htmlOrder', () => ref(null))
+	
 </script>
 
 <template>
-	<div class="shadow-md m-1 bg-gray-50 overflow-y-hidden">
-	<div ref="element" class="font-serif text-md text-justify text-pretty p-5 mt-3 h-[99%] overflow-y-hidden">
-		<table>
-			<tr>
-				<td><span>Поставщик:</span> </td>
-				<td style="padding-inline: 10px;">
-					<input :disabled="isDisabled" class="" placeholder="ИНН" v-model.trim.lazy="orderData.saller.inn" /><br />
-					<input :disabled="isDisabled" placeholder="Название компании"
-						v-model.lazy="orderData.saller.companyName" /><br />
-					<input :disabled="isDisabled" placeholder="Юр.Адресс" v-model.lazy="orderData.saller.legalAddress" /><br />
-					<input :disabled="isDisabled" placeholder="Контактный телефон"
-						v-model.trim.lazy="orderData.saller.mobileNumber" />
-				</td>
-			</tr>
-			<tr>
-				<td>
-					<span>Покупатель:</span>
-				</td>
-				<td style="padding-inline: 10px;">
-					<input :disabled="isDisabled" placeholder="Название компании"
-						v-model.lazy="orderData.buyer.companyName" /><br />
-					<input :disabled="isDisabled" placeholder="Юр.Адресс" v-model.lazy="orderData.buyer.legalAddress" /><br />
-					<input :disabled="isDisabled" placeholder="Контактный телефон"
-						v-model.lazy="orderData.buyer.mobileNumber" /><br />
-				</td>
-			</tr>
-		</table>
+	<A4-page>
 
-		<h1 style="font-weight: 700;" class="font-bold my-2">Заказ на поставку 
-			<span v-if="orderData.orderNumber">{{ orderData.orderNumber }}</span>
-			 от {{ orderData.orderDate }}</h1>
 
-		<table class="table-fixed p-5 mb-5 w-[99%] text-center" id="products">
-			<thead>
-				<th class="w-5 border"><span>№</span></th>
-				<th class="w-50 border"><span>Название продукта</span></th>
-				<th class="w-15 border"><span>Артикул</span></th>
-				<th class="w-10 border"><span>Кол-во</span></th>
-				<th class="w-13 border"><span>Ед. изм.</span></th>
-				<th class="w-15 border"><span>Цена</span></th>
-				<th class="w-20 border"><span>Сумма</span></th>
-				<th class="w-1"><span></span></th>
-			</thead>
-			<tbody>
-				<tr v-for="product in orderData.products">
-					<td class="border">
-						<span>{{ orderData.products.indexOf(product) + 1 }}</span>
+		<div ref="element">
+			<table>
+				<tr>
+					<td><span>Поставщик:</span> </td>
+					<td style="padding-inline: 10px;">
+						<input :disabled="isDisabled" class="" placeholder="ИНН" v-model.trim.lazy="orderData.saller.inn" /><br />
+						<input :disabled="isDisabled" placeholder="Название компании"
+							v-model.lazy="orderData.saller.companyName" /><br />
+						<input :disabled="isDisabled" placeholder="Юр.Адресс" v-model.lazy="orderData.saller.legalAddress" /><br />
+						<input :disabled="isDisabled" placeholder="Контактный телефон"
+							v-model.trim.lazy="orderData.saller.mobileNumber" />
 					</td>
-					<td class="border">
-						<input :disabled="isDisabled" class="w-72" placeholder="Название" v-model.lazy="product.name" />
-					</td>
-					<td class="border">
-						<input :disabled="isDisabled" class="w-21 text-center" placeholder="Артикул"
-							v-model.lazy="product.article" />
-					</td>
-					<td class="border">
-						<input :disabled="isDisabled" class="w-14 text-center" placeholder="Кол-во"
-							v-model.lazy="product.quantity" />
-					</td>
-					<td class="border">
-						<input :disabled="isDisabled" class="w-18 text-center" placeholder="Ед. изм."
-							v-model.lazy="product.units" />
-					</td>
-					<td class="border">
-						<input :disabled="isDisabled" class="w-21 text-center" placeholder="Цена" v-model.lazy="product.price" />
-					</td>
-					<td class="border">
-						<span class="">{{ product.amount }}</span>
-					</td>
+				</tr>
+				<tr>
 					<td>
-						<span :hidden="isDisabled" class="w-[10px] cursor-pointer" @click="removeProduct(product)">
-							<svg class="w-7 h-5 fill-none stroke-neutral-400 hover:stroke-red-400" xmlns="http://www.w3.org/2000/svg"
-								width="32" height="32" viewBox="0 0 24 24">
-								<g class="fill-white stroke-neutral-400 hover:stroke-red-400" stroke-linecap="round"
-									stroke-linejoin="round" stroke-width="3">
-									<circle cx="12" cy="12" r="10" />
-									<path d="m15 9l-6 6m0-6l6 6" />
-								</g>
-							</svg>
-						</span>
+						<span>Покупатель:</span>
 					</td>
-
-				</tr>
-				<tr :hidden="isDisabled">
-					<td @click="addProduct()" colspan="7"
-						class="border text-left text-gray-400 hover:text-gray-700 cursor-pointer">
-						Добавить товар
+					<td style="padding-inline: 10px;">
+						<input :disabled="isDisabled" placeholder="Название компании"
+							v-model.lazy="orderData.buyer.companyName" /><br />
+						<input :disabled="isDisabled" placeholder="Юр.Адресс" v-model.lazy="orderData.buyer.legalAddress" /><br />
+						<input :disabled="isDisabled" placeholder="Контактный телефон"
+							v-model.lazy="orderData.buyer.mobileNumber" /><br />
 					</td>
 				</tr>
-			</tbody>
-		</table>
+			</table>
 
-		<p><span>Всего наименований:{{ orderData.products.length }}, на сумму: 
-			<span v-if="orderData.amount">{{ orderData.amount }} </span>
-			p.</span></p>
-		<p><span class="underline underline-offset-4">{{ orderData.amountWord }}</span></p>
-		<br />
-		<p>
-			<span style="text-align: start;">Менеджер </span>
-			<input :disabled="isDisabled" placeholder="Имя продавца" v-model.lazy="orderData.saller.name" />
-			<span style="text-align: center;">Покупатель</span>
-			<input :disabled="isDisabled" placeholder="Имя покупателя" v-model.lazy="orderData.buyer.name" />
-		</p>
-		<br />
+			<h1 style="font-weight: 700;" class="font-bold my-2">Заказ на поставку
+				<span v-if="orderData.orderNumber">{{ orderData.orderNumber }}</span>
+				от {{ orderData.orderDate }}
+			</h1>
 
-		<textarea :disabled="isDisabled" ref="comment" placeholder="Комментарии" v-model.lazy="orderData.comments"
-			class="w-full h-15 max-h-40" />
-	</div>
-	</div>
+			<table class="table-fixed p-5 mb-5 w-[99%] text-center" id="products">
+				<thead>
+					<th class="w-5 border"><span>№</span></th>
+					<th class="w-50 border"><span>Название продукта</span></th>
+					<th class="w-15 border"><span>Артикул</span></th>
+					<th class="w-10 border"><span>Кол-во</span></th>
+					<th class="w-13 border"><span>Ед. изм.</span></th>
+					<th class="w-15 border"><span>Цена</span></th>
+					<th class="w-20 border"><span>Сумма</span></th>
+					<th class="w-1"><span></span></th>
+				</thead>
+				<tbody>
+					<tr v-for="product in orderData.products">
+						<td class="border">
+							<span>{{ orderData.products.indexOf(product) + 1 }}</span>
+						</td>
+						<td class="border">
+							<input :disabled="isDisabled" class="w-72" placeholder="Название" v-model.lazy="product.name" />
+						</td>
+						<td class="border">
+							<input :disabled="isDisabled" class="w-21 text-center" placeholder="Артикул"
+								v-model.lazy="product.article" />
+						</td>
+						<td class="border">
+							<input :disabled="isDisabled" class="w-14 text-center" placeholder="Кол-во"
+								v-model.lazy="product.quantity" />
+						</td>
+						<td class="border">
+							<input :disabled="isDisabled" class="w-18 text-center" placeholder="Ед. изм."
+								v-model.lazy="product.units" />
+						</td>
+						<td class="border">
+							<input :disabled="isDisabled" class="w-21 text-center" placeholder="Цена" v-model.lazy="product.price" />
+						</td>
+						<td class="border">
+							<span class="">{{ product.amount }}</span>
+						</td>
+						<td>
+							<span :hidden="isDisabled" class="w-[10px] cursor-pointer" @click="removeProduct(product)">
+								<svg class="w-7 h-5 fill-none stroke-neutral-400 hover:stroke-red-400"
+									xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+									<g class="fill-white stroke-neutral-400 hover:stroke-red-400" stroke-linecap="round"
+										stroke-linejoin="round" stroke-width="3">
+										<circle cx="12" cy="12" r="10" />
+										<path d="m15 9l-6 6m0-6l6 6" />
+									</g>
+								</svg>
+							</span>
+						</td>
+
+					</tr>
+					<tr :hidden="isDisabled">
+						<td @click="addProduct()" colspan="7"
+							class="border text-left text-gray-400 hover:text-gray-700 cursor-pointer">
+							Добавить товар
+						</td>
+					</tr>
+				</tbody>
+			</table>
+
+			<p><span>Всего наименований:{{ orderData.products.length }}, на сумму:
+					<span v-if="orderData.amount">{{ orderData.amount }} </span>
+					p.</span></p>
+			<p><span class="underline underline-offset-4">{{ orderData.amountWord }}</span></p>
+			<br />
+			<p>
+				<span style="text-align: start;">Менеджер </span>
+				<input :disabled="isDisabled" placeholder="Имя продавца" v-model.lazy="orderData.saller.name" />
+				<span style="text-align: center;">Покупатель</span>
+				<input :disabled="isDisabled" placeholder="Имя покупателя" v-model.lazy="orderData.buyer.name" />
+			</p>
+			<br />
+
+			<textarea :disabled="isDisabled" ref="comment" placeholder="Комментарии" v-model.lazy="orderData.comments"
+				class="w-full h-15 max-h-40" />
+		</div>
+
+
+	</A4-page>
 </template>
 
 <style lang="css" scoped>
---
+++ frontend/components/ui/A4-page.vue
@@ -0,0 +1,99 @@
+<template>
+	<div class="a4-background">
+		<div class="a4-page_container">
+			<div class="a4-page_document">
+				<slot></slot>
+			</div>
+		</div>
+	</div>
+
+</template>
+
+<!-- <script setup lang="ts">
+import { ref } from 'vue'
+
+const props = defineProps<{
+	element: HTMLElement | null
+}>()
+
+const element: Ref<HTMLElement | null> = ref(null)
+const pages: Ref<HTMLElement[][]> = ref([])
+
+if (props.element) {
+	element.value = props.element
+}
+
+//split elments on pages
+const splitElements = (pageElement: HTMLElement): HTMLElement[][] => {
+	const pages: HTMLElement[][] = []
+	const contentMaxHeight = 1016;
+	let currentPage: HTMLElement[] = []
+	let currentHeight = 0
+
+	if (pageElement.children[0]) {
+		const childrenList = Array.from(pageElement.children?.[0].children) as HTMLElement[]
+
+		childrenList.forEach((el: HTMLElement) => {
+			const elHeight = el.offsetHeight
+
+			if (elHeight + currentHeight <= contentMaxHeight) {
+				currentHeight += elHeight
+				currentPage.push(el)
+			} else {
+				pages.push(currentPage)
+				currentPage = [el]
+				currentHeight = elHeight
+			}
+		})
+
+		pages.push(currentPage)
+	}
+	return pages
+}
+
+//observer
+const elementHeight = ref(0)
+
+watch(element, (el) => {
+	if (!el) return
+
+	const observer = new ResizeObserver(entries => {
+		if (entries[0]) {
+			elementHeight.value = entries[0].contentRect.height
+		}
+	})
+	observer.observe(el)
+
+	onUnmounted(() => observer.disconnect())
+}, { immediate: true, deep: true })
+
+
+watch(elementHeight, (height) => {
+	console.log('Высота изменилась:', height)
+	const pagesQuantity = Math.ceil((elementHeight.value) / 1046)
+	console.log('Количество необходимых страниц: ', pagesQuantity)
+})
+
+</script> -->
+
+<style scoped>
+.a4-background {
+	background-color: #e0e0e0;
+	padding: 20px;
+}
+
+.a4-background .a4-page_container {
+	height: 297mm;
+	width: 210mm;
+	background: white;
+	margin: 0 auto;
+	padding: 10mm;
+	box-sizing: border-box;
+	box-shadow: 0 4px 12px rgba(0, 0, 0, 0.15);
+}
+
+.a4-page_container .a4-page_document {
+	font-size: 14px;
+	font-family: serif;
+}
+</style>
